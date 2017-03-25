ff96b3100373846458fc6eb022232280a977e112
Ryan <jmacrae@clemson.edu>
2017-03-24 22:03:13 -0400

    Initial commit

---
+++ neuralnet.py
@@ -0,0 +1,52 @@
+# Ryan MacRae
+# 3/24/2017
+# Neural Network for XOR
+
+
+import numpy as np
+
+# sigmoid activation function
+def nonlin(x, deriv):
+    if (deriv == True):
+        return x * (1 - x)
+    else: return 1 / (1 + np.exp(-x))
+
+# number of epochs.
+epochs = 1000000
+
+print ("Neural Network for XOR\n")
+
+# input values
+X = np.array([[0, 0], [0, 1], [1, 0],[1, 1]])
+
+# output values
+y = np.array([ [0], [1], [1], [0] ])
+
+# initialize weights randomly with mean 0
+synapse0 = np.random.random(size=(2, 3)) - 1    # Weights on hidden layer inputs
+synapse1 = np.random.random(size=(3, 1)) - 1    # Weights on output layer inputs
+
+for iter in range(epochs):
+    # forward propagation
+    l0 = X
+    l1 = nonlin(np.dot(l0, synapse0), False)
+    layer_2 = nonlin(np.dot(l1, synapse1), False)
+
+    # Error Calculation
+    l1_error = y - layer_2
+
+    # backward propagation
+    l2_delta = l1_error * nonlin(layer_2, True)
+    l1_delta = np.dot(l2_delta, synapse1.T) * nonlin(l1, True)
+
+    # update weights
+    synapse1 += np.dot(l1.T, l2_delta)
+    synapse0 += np.dot(l0.T, l1_delta)
+
+# print results
+print ("Output After Training (%d epochs):" % epochs)
+print (layer_2)
+print ("\n")
+print ("Expected Values: ")
+print (y)
+
